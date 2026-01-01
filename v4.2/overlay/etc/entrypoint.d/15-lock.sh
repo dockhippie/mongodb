d4d95993d40668d8e4bbd781e9d7700bcef4c009
Thomas Boerger <thomas@webhippie.de>
2026-01-02 00:34:43 +0100

    feat: use bash expansion instead of test conditions for variables

--- v4.2/overlay/etc/entrypoint.d/15-lock.sh
+++ v4.2/overlay/etc/entrypoint.d/15-lock.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 declare -x MONGODB_REMOVE_LOCK
-[[ -z "${MONGODB_REMOVE_LOCK}" ]] && MONGODB_REMOVE_LOCK="true"
-
-true
+: "${MONGODB_REMOVE_LOCK:="true"}"
--- v4.4/overlay/etc/entrypoint.d/10-backup.sh
+++ v4.4/overlay/etc/entrypoint.d/10-backup.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 declare -x MONGODB_BACKUP_IGNORE
-[[ -z "${MONGODB_BACKUP_IGNORE}" ]] && MONGODB_BACKUP_IGNORE="(admin|config|local)"
+: "${MONGODB_BACKUP_IGNORE:="(admin|config|local)"}"
 
 declare -x MONGODB_BACKUP_PATH
-[[ -z "${MONGODB_BACKUP_PATH}" ]] && MONGODB_BACKUP_PATH="/var/lib/backup"
+: "${MONGODB_BACKUP_PATH:="/var/lib/backup"}"
 
 declare -x MONGODB_BACKUP_RETENTION
-[[ -z "${MONGODB_BACKUP_RETENTION}" ]] && MONGODB_BACKUP_RETENTION="14"
-
-true
+: "${MONGODB_BACKUP_RETENTION:="14"}"
